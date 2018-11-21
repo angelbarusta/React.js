57d31c4588703142bad4829fd73fe0b56caeb377
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-21 16:46:10 -0600

    ReactDOM ok

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ import ReactDOM from 'react-dom';
 
 //console.log('Hola mundo')
 
-const app = document.getElementById('app')
+const app = document.getElementById('app');
 
-const holamundo = <h1>hola mundo!</h1>;
+const holamundo = <h1>AZ-Tech!!</h1>;
+
 //ReactDOM.render(que voy a realizar, en dónde lo haré);
 ReactDOM.render(holamundo, app);
