2890a5bb4f548c6025a37cd09cfd72d455b542a8
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-23 17:47:28 -0600

    anexo de imagenes az-pets y más

--- src/playlist/componentes/media.js
+++ src/playlist/componentes/media.js
@@ -2,10 +2,20 @@ import React, { Componentes } from 'react';
 
 class Media extends React.Component {
     render() {
+        const styles = {
+            container: {
+                fontSize: 21, // o fontSize:'14px'
+                backgroundColor: '#2ECCFA',
+                color: 'white', //color del texto
+                cursor: 'pointer',
+                width:260,
+                border: '1px solid blue'
+            }
+        }
         return (
-            <div>
+            <div style={ styles.container}>
               <div>
-                  <img src=""
+                  <img src="./imagenes/doky.jpg"
                        alt=""
                        width={260}
                        height={160}
