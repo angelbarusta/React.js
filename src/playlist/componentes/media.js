91e5a08edc14e517cc23b0336cdacde18f754b16
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-23 18:12:11 -0600

    correccion

--- src/playlist/componentes/media.js
+++ src/playlist/componentes/media.js
@@ -1,5 +1,5 @@
 import React, { Componentes } from 'react';
-import './media.css';
+import Mediacss from'./css/media.css';
 class Media extends React.Component {
     render() {
         const styles = {
