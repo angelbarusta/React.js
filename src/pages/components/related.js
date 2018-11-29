1f845dd75eaff81ec09b26659ed4c86f32eac097
Angel Barusta <angel.barusta.95@gmail.com>
2018-11-29 11:40:41 -0600

    estetica de la app web

---
+++ src/pages/components/related.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../../imagenes/az_tech_logo.png';
+import './related.css';
+
+  const Related =(props)=>{
+    return(
+        <div className="Related">
+            <img src={Logo} width={100} height={100}/>
+        </div>
+    )
+}
+export default Related;
